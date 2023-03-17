53fc06c212678c80bbfe6f50f7f07c0b6afffa81
Helly Zhang <v-helzha@microsoft.com>
2023-03-17 15:56:32 +0800

    test

--- src/a.ts
+++ src/a.ts
@@ -1,3 +1,6 @@
 import { Env } from "./env";
+if(`${process.env.M365_USERNAME}` == 'abc$abc'){
+    console.log("correct")
+}
 console.log(`M365:${process.env.M365_USERNAME}`);
 console.log(`M365B:${Env.username}`);
