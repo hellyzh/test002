87a4cb2f6f51333de47f918a109caa2c97e44ef0
Helly Zhang <v-helzha@microsoft.com>
2023-04-01 08:52:44 +0800

    test

--- src/a.ts
+++ src/a.ts
@@ -1,10 +1,10 @@
 import { Env } from "./env";
-console.log(`M365:${process.env.M365_USERNAME}`);
-const a=`${process.env.M365_USERNAME}`
+console.log(`M365:${process.env.AZURE_ACCOUNT_NAME}`);
+const a=`${process.env.AZURE_ACCOUNT_NAME}`
 // console.log(`M365B:${Env.username}`);
-if(a == 'helly1-test'){
+if(a == 'helly1'){
     console.log("yes")
-}else if(a == 'helly2-test'){
+}else if(a == 'helly2'){
     console.log("yes2")
 }else{
     console.log("no")
